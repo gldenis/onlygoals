9b5c946139ed0b8a2c016dedc1a09ce9b5995024
Denis Glinkyi <gl.denis1996@gmail.com>
2024-02-13 07:21:33 +0200

    FIX: game card start

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -47,10 +47,137 @@ import IconCharts from '@/components/icons/hero/IconCharts.vue'
         </div>
       </div>
     </section>
+    <section class="games">
+      <div class="container">
+        <div class="game-card">
+          <div class="game-meta">
+            <div class="game-meta__part">
+              <div class="game-meta__logo">
+                <img src="@/assets/img/content/real-madrid.png" alt="">
+              </div>
+              <div class="game-meta__inner">
+                <div class="game-meta__name">Real Madrid</div>
+                <div class="game-meta__last-games-status last-games-status__list">
+                  <div class="last-games-status__item last-games-status__item--lose"></div>
+                  <div class="last-games-status__item last-games-status__item--win"></div>
+                  <div class="last-games-status__item last-games-status__item--draw"></div>
+                  <div class="last-games-status__item last-games-status__item--win"></div>
+                  <div class="last-games-status__item last-games-status__item--win"></div>
+                </div>
+              </div>
+              <div class="game-meta__current-score">3</div>
+            </div>
+            <div class="game-meta__current-score current-score">
+              <div class="current-score__item">3</div>
+              :
+              <div class="current-score__item">2</div>
+            </div>
+            <div class="game-meta__part game-meta__part--revert">
+              <div class="game-meta__logo">
+                <img src="@/assets/img/content/real-madrid.png" alt="">
+              </div>
+              <div class="game-meta__inner">
+                <div class="game-meta__name">Real Madrid</div>
+                <div class="game-meta__last-games-status last-games-status__list">
+                  <div class="last-games-status__item last-games-status__item--lose"></div>
+                  <div class="last-games-status__item last-games-status__item--win"></div>
+                  <div class="last-games-status__item last-games-status__item--draw"></div>
+                  <div class="last-games-status__item last-games-status__item--win"></div>
+                  <div class="last-games-status__item last-games-status__item--win"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
   </main>
 </template>
 
 <style lang="scss" scoped>
+.game-card {
+  border-radius: 4px;
+  background: var(--neutral-right-84, rgba(25, 27, 34, 0.84));
+  backdrop-filter: blur(4px);
+  padding: 2px;
+}
+
+.game-meta {
+  border-radius: 2px;
+  padding: 0 4px;
+  width: 400px;
+  height: 38px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 4px;
+  background: linear-gradient(249deg, rgba(255, 255, 255, 0.00) 0%, rgba(255, 255, 255, 0.08) 100%);
+
+  &__part {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+
+    &--revert {
+      flex-direction: row-reverse;
+    }
+  }
+
+  &__name {
+    overflow: hidden;
+    color: var(--Neutral-1, #E9EAEC);
+    text-overflow: ellipsis;
+    font-size: 13px;
+    font-weight: 700;
+    line-height: 132%;
+  }
+}
+
+.current-score {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+
+  &__item {
+    border-radius: 4px;
+    background: var(--neutral-left-12, rgba(255, 255, 255, 0.12));
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 128%;
+    text-align: center;
+    color: var(--gray-1);
+    width: 25px;
+    height: 28px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+
+.last-games-status {
+  &__list {
+    display: flex;
+    gap: 6px;
+  }
+
+  &__item {
+    width: 4px;
+    height: 4px;
+    border-radius: 50%;
+
+    &--win {
+      background: var(--green-5);;
+    }
+    &--lose {
+      background: var(--cd3d00);
+    }
+    &--draw{
+      background: var(--gray-3);
+    }
+  }
+}
 .hero-tiles {
   display: flex;
   flex-wrap: wrap;
@@ -102,5 +229,9 @@ import IconCharts from '@/components/icons/hero/IconCharts.vue'
       line-height: 128%;
     }
   }
+}
+
+.game-card {
+
 }
 </style>
