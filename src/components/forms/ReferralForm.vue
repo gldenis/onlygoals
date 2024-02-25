33fd05689f1ace7a456bcc68fe9c50aa0b4edde9
Denis Glinkyi <gl.denis1996@gmail.com>
2024-02-25 12:13:23 +0200

    FEAT: invite

--- src/components/forms/ReferralForm.vue
+++ src/components/forms/ReferralForm.vue
@@ -22,7 +22,7 @@ const formSubmit = handleSubmit(values => {
 })
 
 const authStore = useAuthStore()
-
+const inviteType = ref('email')
 </script>
 
 <template>
@@ -31,10 +31,20 @@ const authStore = useAuthStore()
       <div class="form__title form__title--centered">Приглашение пользователя</div>
     </div>
     <div class="form__group">
+      <div class="form-referral__radio">
+        <label class="form-referral__radio-item">
+          <input type="radio" class="form-referral__radio-input" name="type" value="phone" v-model="inviteType">
+          <span class="form-referral__radio-box">По SMS</span>
+        </label>
+        <label class="form-referral__radio-item">
+          <input type="radio" class="form-referral__radio-input" name="type" value="email" v-model="inviteType">
+          <span class="form-referral__radio-box">По эл. почте</span>
+        </label>
+      </div>
       <BaseInput type="text"
                  v-model="email"
                  v-bind="emailAttrs"
-                 placeholder="Email"
+                 :placeholder="inviteType === 'email' ? 'Email' : 'Номер телефона'"
                  :error="errors.email"/>
     </div>
     <button class="btn btn--primary">Сменить</button>
@@ -45,5 +55,42 @@ const authStore = useAuthStore()
 </template>
 
 <style scoped lang="scss">
+.form-referral__radio {
+  border-radius: toRem(4);
+  border: 1px solid rgba(255, 255, 255, 0.12);
+  background:  rgba(25, 27, 34, 0.12);
+  padding: toRem(4);
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch;
+  gap: toRem(4);
+  height: toRem(40);
 
+  &-input {
+    display: none;
+  }
+
+  &-item {
+    width: calc((100%  - 4px) / 2);
+
+  }
+
+  &-box {
+    border-radius: 4px;
+    background: rgba(255, 255, 255, 0.12);
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #E9EAEC;
+    font-size: toRem(14);
+    font-weight: 400;
+    line-height: 150%; /* 21px */
+  }
+
+  &-input:checked + &-box {
+    background: #E9EAEC;
+    color: #191B22;
+  }
+}
 </style>
